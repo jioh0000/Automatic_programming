08e0d620d00037c629d71d7589fd2f90a34d44c4
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-04-26 21:59:14 +0900

    only works on Windows

---
+++ image_position_finding.py
@@ -0,0 +1,9 @@
+# imagePositionFinding = 0 변수
+# IMAGEPOSITIONFINDING = 0 상수
+
+import pyautogui as pg
+
+imageLocation = pg.locateOnScreen('naver.png') # 이 이미지가 있는 위치 가져옴
+point = pg.center(imageLocation)
+print(imageLocation[0], imageLocation[1]) # Left = x, top = y, width = 너비, height = 높이
+
